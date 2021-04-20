e560438d52318249533639cd2e313d314da5ba6f
Andrii_Ostapenko1 <truegreg13@gmail.com>
2021-04-20 14:57:01 +0300

    use TimeUnit

--- src/test/java/delu/cons/edu/AppMock.java
+++ src/test/java/delu/cons/edu/AppMock.java
@@ -7,21 +7,22 @@ import delu.cons.edu.signal.SignalImpl;
 
 import java.util.concurrent.BlockingDeque;
 import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.TimeUnit;
 
 public class AppMock {
 
     public static void main(String[] args) {
-        SignalFilter signalFilter = new SignalFilter(50_000, 1_000_000_000);
+        SignalFilter signalFilter = new SignalFilter(100, TimeUnit.SECONDS.toNanos(1));
 
         BlockingDeque<Signal> inputSignals = new LinkedBlockingDeque<>();
         SignalSourceMock signalSourceMock = new SignalSourceMock(1, inputSignals);
 
         SignalMockGeneratorTask<Signal> signalSignalMockGeneratorTask
-                = new SignalMockGeneratorTask<>(2_000_000, inputSignals, SignalImpl::new, 45);
+                = new SignalMockGeneratorTask<>(400_000_000, inputSignals, SignalImpl::new, 25);
         Thread signalMockGeneratorTaskTread = new Thread(signalSignalMockGeneratorTask, "DSP - Signal Mock Generator");
         signalMockGeneratorTaskTread.start();
 
-        int signalHandlerThreads = 20;
+        int signalHandlerThreads = 1;
         for (int i = 0; i < signalHandlerThreads; i++) {
             SignalHandlerTask signalHandlerTask = new SignalHandlerTask(signalFilter, signalSourceMock);
             Thread signalHandlerTaskThread = new Thread(signalHandlerTask, "DSP " + i + "- Signal Handler");
